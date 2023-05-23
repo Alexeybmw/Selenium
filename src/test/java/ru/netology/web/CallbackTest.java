53b6212ba92bb4310b0fafe77a6e6c4e497e008e
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-05-23 21:07:04 +0300

    falling test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.Color;
 
 import java.util.List;
 
@@ -28,7 +29,7 @@ class CallbackTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
@@ -52,5 +53,44 @@ class CallbackTest {
 
 
     }
+    @Test
+    void shouldTestV2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Volkov Vasiliy");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79260321212");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+
+
+    }
+    @Test
+    void shouldTestV3() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Волков Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79260321");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+
+
+    }@Test
+    void shouldTestV4() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Волков Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79260321212");
+        driver.findElement(By.cssSelector("button.button_view_extra")).click();
+        WebElement invalid = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text"));
+        String color = invalid.getCssValue("color");
+        String expected = Color.fromString(color).asHex();
+        String actual = "#ff5c5c";
+        assertEquals(expected, actual);
+
+
+    }
+
 
 }
